5a54639e9fab633b167983220108a59f85a4393e
Vincent Cubit <vinniecubit@yahoo.com>
2021-10-16 11:29:53 -0500

    Initial

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -5,15 +5,51 @@
 //  Created by Vincent Cubit on 10/16/21.
 //
 
+
 import UIKit
+
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var changeName: UITextField!
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        UserDefaults.standard.setValue("Joe", forKey: "Name")
+        
+
     }
+    
+    
+    @IBAction func saveName(_ sender: Any) {
+        
+        
+        let text = self.changeName.text
+        
+        
+        if let value = text {
+            self.nameLabel.text = value
+        } else {
+            print("Value is nil")
+        }
+        
+        
+    }
+    
 
-
+    @IBAction func loadOriginal(_ sender: Any) {
+        
+        let original = UserDefaults.standard.string(forKey: "Name")
+        
+        self.nameLabel.text = original
+        
+    }
+    
+    
 }
 
